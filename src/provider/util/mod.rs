2cfe17a9424df63d8398476fcb59de5e5437ef9f
François Garillot <francois@garillot.net>
2024-02-23 19:33:59 -0500

    WIP: Jolt msm optimizations

--- src/provider/util/mod.rs
+++ src/provider/util/mod.rs
@@ -1,14 +1,183 @@
 //! Utilities for provider module.
 pub(in crate::provider) mod fb_msm;
 pub mod msm {
+  use ff::PrimeField as _;
+use group::Group;
   use halo2curves::msm::best_multiexp;
   use halo2curves::CurveAffine;
+  use rayon::prelude::*;
+  use bitvec::{array::BitArray, order::Lsb0};
 
   // this argument swap is useful until Rust gets named arguments
   // and saves significant complexity in macro code
   pub fn cpu_best_msm<C: CurveAffine>(bases: &[C], scalars: &[C::Scalar]) -> C::Curve {
-    best_multiexp(scalars, bases)
+    let max_num_bits = scalars
+          .par_iter()
+          .map(|s| {
+            // in practive, this is how every field of interest is implemented
+            assert_eq!(std::mem::size_of::<C::Scalar>(), std::mem::size_of::<[u64; 4]>());
+            let bytes = s.to_repr();
+            let limbs: [u64; 4] = unsafe { std::mem::transmute_copy(&bytes) };
+            
+            let le_array = BitArray::<_, Lsb0>::new(limbs);
+            le_array.len() - le_array.trailing_zeros()
+          })
+          .max()
+          .unwrap();
+
+        let map_field_elements_to_u64 = |field_elements: &[C::Scalar]| {
+          field_elements
+            .iter()
+            .map(|field_element| {
+              // in practive, this is how every field of interest is implemented
+              assert_eq!(std::mem::size_of::<C::Scalar>(), std::mem::size_of::<[u64; 4]>());
+              let bytes = field_element.to_repr();
+              let limbs: [u64; 4] = unsafe { std::mem::transmute_copy(&bytes) };
+              limbs[0]
+            })
+            .collect()
+        };
+
+        match max_num_bits {
+          0 => <C::Curve as Group>::identity(),
+          1 => {
+            let scalars_u64: Vec<_> = map_field_elements_to_u64(scalars);
+            msm_binary(bases, &scalars_u64)
+          }
+          2..=10 => {
+            let scalars_u64: Vec<_> = map_field_elements_to_u64(scalars);
+            msm_small(bases, &scalars_u64, max_num_bits)
+          }
+          11..=64 => {
+            let scalars_u64: Vec<_> = map_field_elements_to_u64(scalars);
+            msm_u64_wnaf(bases, &scalars_u64, max_num_bits)
+          }
+          _ => best_multiexp(scalars, bases),
+        }
+
   }
+
+  fn msm_binary<C: CurveAffine>(bases: &[C], scalars: &[u64]) -> C::Curve {
+    scalars
+      .iter()
+      .zip(bases)
+      .filter(|(&scalar, _base)| scalar != 0)
+      .map(|(_scalar, base)| base)
+      .fold(C::Curve::identity(), |sum, base| sum + base)
+  }
+
+  fn msm_small<C: CurveAffine>(
+    bases: &[C],
+    scalars: &[u64],
+    max_num_bits: usize,
+  ) -> C::Curve {
+    let num_buckets: usize = 1 << max_num_bits;
+    // Assign things to buckets based on the scalar
+    let mut buckets: Vec<C::Curve> = vec![<C::Curve as Group>::identity(); num_buckets];
+    scalars
+      .iter()
+      .zip(bases)
+      .filter(|(&scalar, _base)| scalar != 0)
+      .for_each(|(&scalar, base)| {
+        buckets[scalar as usize] += base;
+      });
+
+    let mut result = <C::Curve as Group>::identity();
+    let mut running_sum = <C::Curve as Group>::identity();
+    buckets.iter().skip(1).rev().for_each(|bucket| {
+      running_sum += bucket;
+      result += running_sum;
+    });
+    result
+  }
+
+  // Adapted from the Jolt implementation, which is in turn adapted from the ark_ec implementation
+  fn msm_u64_wnaf<C: CurveAffine>(
+    bases: &[C],
+    scalars: &[u64],
+    max_num_bits: usize,
+  ) -> C::Curve {
+    use std::cmp::Ordering;
+
+    let c = if bases.len() < 32 {
+      3
+    } else {
+      // ~= ln(bases.len()) + 2
+      (bases.len().ilog2() as usize * 69 / 100) + 2
+    };
+
+    let digits_count = (max_num_bits + c - 1) / c;
+    let radix: u64 = 1 << c;
+    let window_mask: u64 = radix - 1;
+
+    let scalar_digits = scalars
+      .into_par_iter()
+      .flat_map_iter(|&scalar| {
+        let mut carry = 0u64;
+        (0..digits_count).into_iter().map(move |i| {
+          // Construct a buffer of bits of the scalar, starting at `bit_offset`.
+          let bit_offset = i * c;
+          let bit_idx = bit_offset % 64;
+          // Read the bits from the scalar
+          let bit_buf = scalar >> bit_idx;
+          // Read the actual coefficient value from the window
+          let coef = carry + (bit_buf & window_mask); // coef = [0, 2^r)
+
+          // Recenter coefficients from [0,2^c) to [-2^c/2, 2^c/2)
+          carry = (coef + radix / 2) >> c;
+          let mut digit = (coef as i64) - (carry << c) as i64;
+
+          if i == digits_count - 1 {
+            digit += (carry << c) as i64;
+          }
+          digit
+        })
+      })
+      .collect::<Vec<_>>();
+    let zero = <C::Curve as Group>::identity();
+
+    let window_sums: Vec<_> = (0..digits_count)
+      .into_par_iter()
+      .map(|i| {
+        let mut buckets = vec![zero; 1 << c];
+        for (digits, base) in scalar_digits.chunks(digits_count).zip(bases) {
+          // digits is the digits thing of the first scalar?
+          let scalar = digits[i];
+          match 0.cmp(&scalar) {
+            Ordering::Less => buckets[(scalar - 1) as usize] += base,
+            Ordering::Greater => buckets[(-scalar - 1) as usize] -= base,
+            Ordering::Equal => (),
+          }
+        }
+
+        let mut running_sum = <C::Curve as Group>::identity();
+        let mut res = <C::Curve as Group>::identity();
+        buckets.iter().rev().for_each(|b| {
+          running_sum += b;
+          res += &running_sum;
+        });
+        res
+      })
+      .collect();
+
+    // We store the sum for the lowest window.
+    let lowest = *window_sums.first().unwrap();
+
+    // We're traversing windows from high to low.
+    let result = lowest
+      + window_sums[1..]
+        .iter()
+        .rev()
+        .fold(zero, |mut total, sum_i| {
+          total += sum_i;
+          for _ in 0..c {
+            total = total.double();
+          }
+          total
+        });
+    result
+  }
+
 }
 
 pub mod iterators {
